b2225d54256002fc98eb8c3a9e55826da303e4b6
Rupal711 <rupal@gkmit.co>
2024-08-22 00:11:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,5 @@ provider "aws" {
 
 # Genrate Aws key pair 
 resource "aws_key_pair" "my_key_pair" {
-  key_name   = "my-key-pair1"            # Name for your key pair
-  public_key = file("/.ssh/id_ed25519.pub") # Path to your public key file
+  key_name   = "my-key-pair1"            # Name for your key pai
 }
