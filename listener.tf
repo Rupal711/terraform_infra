ccff340ed1883108f0ea93c611cd48dd9b8c61f6
rupal soni <rupalsoni518@gmail.com>
2024-08-22 10:08:17 +0530

    chnages

--- listener.tf
+++ listener.tf
@@ -26,6 +26,19 @@ resource "aws_lb_target_group" "test1" {
     unhealthy_threshold = 3
   }
 }
+resource "aws_lb_target_group" "test2" {
+  name     = "tf-example-lb-tg2"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc2.id
+   health_check {
+    path                = "/app3"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.test.arn
   port              = 443
@@ -55,6 +68,22 @@ resource "aws_lb_listener" "httpss" {
    forward {
       target_group {
         arn = aws_lb_target_group.test1.arn
+      }
+    }
+  }
+}
+resource "aws_lb_listener" "httpss" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:699475925558:certificate/61f37e2c-9205-4c82-be9c-380bd775c479"
+
+  default_action {
+    type = "forward"
+   forward {
+      target_group {
+        arn = aws_lb_target_group.test2.arn
       }
     }
   }
--- target_group_attach.tf
+++ target_group_attach.tf
@@ -9,3 +9,8 @@ resource "aws_lb_target_group_attachment" "test" {
   target_id        = aws_instance.ins3.id
   port             = 8080
 }
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test2.arn
+  target_id        = aws_instance.ins3.id
+  port             = 3000
+}
